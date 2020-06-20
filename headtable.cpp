eddb77c1b127b84c63bfb0ac101c8cf0781e4353
wickdchromosome <capsule.edu@gmail.com>
2020-06-20 02:33:18 -0700

    Added automated padding estimation per column

--- headtable.cpp
+++ headtable.cpp
@@ -12,24 +12,77 @@
 using namespace std;
 
 
-vector<string> read_file(string& filename) {
+vector<vector<string>> read_file(string& filename, int& linelimit) {
+
+	/* Returns a vector of string vectors, each 
+	internal vector containing a single column*/
 
 	ifstream infile(filename);
+	vector<vector<string>> fullfile;
 
-	vector<string>lines;
 	string line;
+	int linec = 0;
+	int numcols = 0;
+
 	//Fill main vector with lines 
 	while(getline(infile, line)) {
 
-		lines.push_back(line);	
+		//split the string using boost
+		vector<string> splitrow;
+		string delim = ",";
+		boost::split(splitrow, line, boost::is_any_of(delim));
+
+		//if this is the first row, allocate the
+		//a vector for each column
+		if (linec == 0) {
+	
+			numcols = splitrow.size();
+			//cout << "Found number of columns to be " << numcols << endl;
+			//Allocate a vector in the main vector for each column
+			for (int i = 0; i < numcols; i++) {
+
+				vector<string> col;
+				fullfile.push_back(col);
+
+			}	
+
+		}
+
+		//now store the correct value in each vector
+		for (int i = 0; i < numcols; i++) {
+		
+			fullfile.at(i).push_back(splitrow.at(i));
+
+		}
+
+		linec += 1;
+		if (linec == linelimit) break ;
+
 
 	}
+	
+	return fullfile;
 
-	return lines;
 
 }
 
+int getcolwidth (vector<string> incol) {
 
+	/*Finds the width that should be used in order to have enough padding
+	for all columns to display correctly*/
+
+	//get the maximum string length in this column
+	int maxdatasize = 0;
+	for (auto& data : incol) {
+
+		int datasize = data.length();
+		if (datasize > maxdatasize) maxdatasize = datasize;
+		
+	}
+
+	return maxdatasize + 3;
+	
+}
 
 int main(int argc, char* argv[]) {
 	
@@ -37,37 +90,44 @@ int main(int argc, char* argv[]) {
 	string filename = argv[1];
 	int linelimit = stoi(argv[2]);
 	const char separator    = ' ';
-	const int nameWidth     = 28;
-	const int numWidth      = 8;
 	int linec = 0; //for storing how many lines we are done with
 
-	//Read in the whole file
-	auto filevect = read_file(filename);
+	//read in the whole file
+	auto filevect = read_file(filename, linelimit);
+	int ncols = filevect.size();
+	//cout << "Read in " << ncols << " columns \n";
 
-	//Iterate over the whole file row by row
-	for (auto& row : filevect) {
 
-		//lets quit if we are done printing
-		if (linec > linelimit) return 0;
+	//here is a vector containing the padding values
+	vector<int> paddings;
 
-		//split the string using boost
-		vector<string> splitrow;
-		string delim = ",";
-		boost::split(splitrow, row, boost::is_any_of(delim));
+	//iterate over each column
+	for (int j = 0; j < ncols; j++) {
 
-		//now iterate over each row and print each time for that row	
-		for(auto& col : splitrow) {
-
-			cout << left << setw(nameWidth) << setfill(separator) << col;
-
-		}
-
-		linec += 1;
-		cout << endl;
+		//get the padding size for this row
+		int paddingw = getcolwidth(filevect.at(j));
+		paddings.push_back(paddingw);
 
 	}
 		
 
+	//now lets print the results for each row
+	for (int i = 0; i < linelimit; i++) {
+
+		//lets quit if we are done printing
+		if (linec > linelimit) break;
+
+		//now iterate over each row and print each column with the
+		//required padding
+		for (int j = 0; j < ncols; j++) {
+
+			cout << left << setw(paddings.at(j)) << setfill(separator) << filevect.at(j).at(i);
+
+		}	
+
+		linec += 1;
+		cout << endl;
+	}
 
 
 }
